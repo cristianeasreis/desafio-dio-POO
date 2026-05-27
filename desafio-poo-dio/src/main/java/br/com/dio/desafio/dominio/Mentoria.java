35ec1222505f99d160dec0b06dc84e1f8b5b90a6
CRISTIANE ARAUJO SOUZA DOS REIS <>
2026-05-27 12:46:42 -0300

    feat: atualiza atributos de curso e mentoria

--- desafio-poo-dio/src/main/java/br/com/dio/desafio/dominio/Mentoria.java
+++ desafio-poo-dio/src/main/java/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,13 +1,32 @@
 package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Mentoria extends Conteudo {
+    private String titulo;
+    private String descricao;
     private LocalDate data;
 
     @Override
     public double calcularXp() {
         return XP_PADRAO + 20d;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public LocalDate getData() {
@@ -16,6 +35,25 @@ public class Mentoria extends Conteudo {
 
     public void setData(LocalDate data) {
         this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Mentoria mentoria = (Mentoria) o;
+        return Objects.equals(titulo, mentoria.titulo)
+                && Objects.equals(descricao, mentoria.descricao)
+                && Objects.equals(data, mentoria.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, descricao, data);
     }
 }
 
